d7f4cdf60114cec756a00ee4453716b7099e7d03
suchetsingh <ssingh21be20@gmail.com>
2024-01-11 21:40:45 +0530

    ba

--- index.js
+++ index.js
@@ -1,23 +1,51 @@
-let intemsContainerElement = document.querySelector(".items-container");
+let bagItems = [];
+function addToBag(itemId) {
+  bagItems.push(itemId);
+  displayBagIcon();
+}
 
-let innerHTML='';
-items.forEach(item=>{
-    innerHTML+=`
-    <div class="item-container">
-    <img class="item-image" src="${item.image}" alt="item-image">
-    <div class="rating">
-        ${item.rating.stars} ⭐ | ${item.rating.count}
-    </div>
-    <div class="company-name">${item.company}</div>
-    <div class="item-name">${item.item_name}</div>
-    <div class="price">
-        <span class="current-price">Rs ${item.current_price}</span>
-        <span class="original-price">Rs ${item.original_price}</span>
-        <span class="discount">(${item.discount_percentage}% OFF)</span>
-    </div>
-    <button class="btn-add-bag">Add to Bag</button>
-</div>`
-});
+function displayBagIcon() {
+    // Select the element with the class "bag-item-count"
+    let bagItemCountElement = document.querySelector(".bag-item-count");
+  
+    // Check if there are items in the bagItems array
+    if (bagItems.length > 0) {
+      // If there are items, make the element visible
+      bagItemCountElement.style.visibility = 'visible';
+  
+      // Set the text content of the element to the number of items in the array
+      bagItemCountElement.innerText = bagItems.length;
+    } else {
+      // If there are no items, hide the element
+      bagItemCountElement.style.visibility = 'hidden';
+    }
+  }
+  
+displayItemsOnHomepage();
+function displayItemsOnHomepage() {
+  let intemsContainerElement = document.querySelector(".items-container");
+
+  let innerHTML = "";
+  items.forEach((item) => {
+    innerHTML += `
+        <div class="item-container">
+        <img class="item-image" src="${item.image}" alt="item-image">
+        <div class="rating">
+            ${item.rating.stars} ⭐ | ${item.rating.count}
+        </div>
+        <div class="company-name">${item.company}</div>
+        <div class="item-name">${item.item_name}</div>
+        <div class="price">
+            <span class="current-price">Rs ${item.current_price}</span>
+            <span class="original-price">Rs ${item.original_price}</span>
+            <span class="discount">(${item.discount_percentage}% OFF)</span>
+        </div>
+        <button class="btn-add-bag" onClick="addToBag(${item.id})">Add to Bag</button>
+    </div>`;
+  });
+  intemsContainerElement.innerHTML = innerHTML;
+}
+
 // let item = {
 //     item_image:'images/1.jpg',
 //     rating:{
@@ -30,5 +58,3 @@ items.forEach(item=>{
 //     original_price:1045,
 //     discount_percentage:42
 // }
-
-intemsContainerElement.innerHTML = innerHTML;
